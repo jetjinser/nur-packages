d9cf10c460f2db381aef7ddc68824f1da154ec54
jinser <cmdr.jv@gmail.com>
2022-07-29 13:26:18 +0800

    v2raya

--- pkgs/v2raya/default.nix
+++ pkgs/v2raya/default.nix
@@ -1,30 +1,16 @@
-{
-  lib,
-  fetchFromGitHub,
-  mkYarnPackage,
-  buildGoModule,
-  makeWrapper,
-  v2ray,
-  v2ray-geoip,
-  v2ray-domain-list-community,
-  symlinkJoin,
-}: let
+{ lib, fetchFromGitHub, mkYarnPackage, buildGo117Module }:
+let
   pname = "v2raya";
-  version = "1.5.8.1";
-
+  version = "48cc58d54727ea4beaadea5c0fb4150356809b72";
   src = fetchFromGitHub {
-    owner = "v2rayA";
+    owner = "SCP-2000";
     repo = "v2rayA";
-    rev = "v${version}";
-    sha256 = "sha256-zx+AI7AB/Y/E943lAX06NqOd4CrS+YGSmVoYzJO6iHw=";
+    rev = version;
+    sha256 = "sha256-lLXg4KkdbOx8Sw8CPIQBkYpFJ5cX3immP0XcXkxKu78=";
   };
-
   web = mkYarnPackage {
     inherit pname version;
     src = "${src}/gui";
-    yarnNix = ./yarn.nix;
-    packageJSON = ./package.json;
-    yarnLock = ./yarn.lock;
     buildPhase = ''
       ln -s $src/postcss.config.js postcss.config.js
       OUTPUT_DIR=$out yarn --offline build
@@ -34,30 +20,17 @@
     dontFixup = true;
   };
 in
-  buildGoModule {
-    inherit pname version;
-    src = "${src}/service";
-    vendorSha256 = "sha256-88tHCnx6qEsRHm5xNyroRL+fh+nsVeWTEqCPEZM9zX0=";
-    subPackages = ["."];
-    nativeBuildInputs = [makeWrapper];
-    preBuild = ''
-      cp -a ${web} server/router/web
-    '';
-
-    postInstall = ''
-      wrapProgram $out/bin/v2rayA \
-        --prefix PATH ":" "${lib.makeBinPath [v2ray.core]}" \
-        --prefix XDG_DATA_DIRS ":" ${symlinkJoin {
-        name = "assets";
-        paths = [v2ray-geoip v2ray-domain-list-community];
-      }}/share
-    '';
-
-    meta = with lib; {
-      description = "A Linux web GUI client of Project V which supports V2Ray, Xray, SS, SSR, Trojan and Pingtunnel";
-      homepage = "https://github.com/v2rayA/v2rayA";
-      mainProgram = "v2rayA";
-      license = licenses.agpl3Only;
-      maintainers = with lib.maintainers; [shanoaice];
-    };
-  }
+buildGo117Module {
+  inherit pname version;
+  src = "${src}/service";
+  vendorSha256 = "sha256-ALy3Co461N1MJpiEUnjOoNswY4TkE9W8nfeNRNLRyfQ=";
+  subPackages = [ "." ];
+  preBuild = ''
+    cp -a ${web} server/router/web
+  '';
+  meta = with lib; {
+    description = "A Linux web GUI client of Project V which supports V2Ray, Xray, SS, SSR, Trojan and Pingtunnel";
+    homepage = "https://github.com/v2rayA/v2rayA";
+    license = licenses.agpl3;
+  };
+}
